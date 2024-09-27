c63a29474706ffe38e24c03ff8ebd25bf2989602
Tihonov Artyom <pop1212.1212@yandex.ru>
2024-09-28 02:23:57 +0300

    v2

--- src/main/java/com/example/demo/controllers/InputPageController.java
+++ src/main/java/com/example/demo/controllers/InputPageController.java
@@ -17,7 +17,9 @@ public class InputPageController {
         return "inputPage";
     }
     @PostMapping
-    public String inputSave(@RequestParam(value = "text", required = false) String text){
+    public String inputSave(@RequestParam(value = "text", required = false) String text
+            ,@RequestParam(value = "text2", required = false) String text2, Model model){
+        System.out.println(text2);
         if(text != null){
             try(FileWriter writer = new FileWriter("note.txt", false)){
                 writer.append(text);
@@ -27,6 +29,7 @@ public class InputPageController {
                 System.out.println(e.getMessage());
             }
         }
+        model.addAttribute("text2", text2);
         return "inputPage";
     }
 
